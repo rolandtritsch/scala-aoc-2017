997f32b0f5d84cdb91cfd91e81deaa6061784ece
Roland Tritsch <roland@gonito.com>
2017-12-06 16:30:14 +0000

    Day 1: Refactoring. Part1/2.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,8 +2,8 @@ package aoc
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(s"Day1: Captcha -> ${Day1.captcha(Day1.in)}")
-    println(s"Day1: Captcha2 -> ${Day1.captcha2(Day1.in)}")
+    println(s"Day1: Captcha -> ${Day1.Part1.captcha(Day1.in)}")
+    println(s"Day1: Captcha2 -> ${Day1.Part2.captcha(Day1.in)}")
     println(s"Day2: CheckSum -> ${Day2.checksum(Day2.in)}")
     println(s"Day2: CheckSum2 -> ${Day2.checksum2(Day2.in)}")
     println(s"Day3: CalcDistance -> ${Day3.Part1.calcDistanceFromLocToCenter(Day3.in, Day3.Part1.initGrid(Day3.in))}")
@@ -12,5 +12,7 @@ object Main {
     println(s"Day4: CountValid2 -> ${Day4.Part2.countValid(Day4.in)}")
     println(s"Day5: CountSteps -> ${Day5.Part1.countSteps(Day5.in, 0)}")
     println(s"Day5: CountSteps2 -> ${Day5.Part2.countSteps(Day5.in, 0)}")
+    println(s"Day6: DetectLoops -> ${Day6.Part1.detectLoop(Day6.in)._1}")
+    println(s"Day6: DetectLoops -> ${Day6.Part1.detectLoop(Day6.in)._2}")
   }
 }
--- src/test/scala/Day1Check.scala
+++ src/test/scala/Day1Check.scala
@@ -14,7 +14,7 @@ class Day1Check extends PropSpec with GeneratorDrivenPropertyChecks with Matcher
   property("Any string of equal numbers should be <number>*<length of the string") {
     forAll(equalStrings) {in =>
       val expectedResult = in.head.toString.toInt * in.length
-      Day1.captcha(in) shouldBe expectedResult
+      Day1.Part1.captcha(in) shouldBe expectedResult
       //Day1.captcha2(in) shouldBe expectedResult
     }
   }
