e797f83ca00720a08deb2d0aa2c6a647a226bb31
Roland Tritsch <roland@gonito.com>
2017-12-06 16:29:28 +0000

    Day 6: Final commit.

---
+++ src/main/scala/Day6.scala
@@ -0,0 +1,39 @@
+package aoc
+
+object Day6 {
+
+  val in = List(10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6)
+
+  object Part1 {
+    def redistribute(banks: List[Int]): List[Int] = {
+      val maxBlocks = banks.max
+      val mostBlocksIndex = banks.indexWhere(b => b == maxBlocks)
+      var newBanks = banks.updated(mostBlocksIndex, 0)
+      for {
+        i <- 1 to maxBlocks
+        n = ((mostBlocksIndex + i) % banks.size)
+      } {
+        newBanks = newBanks.updated(n, newBanks(n) + 1)
+      }
+      newBanks
+    }
+
+    def detectLoop(banks: List[Int]): (Int, Int) = {
+      def go(distribution: List[Int], sofar: List[List[Int]], step: Int): (Int, Int) = {
+        val newDistribution = redistribute(distribution)
+        if (sofar.contains(newDistribution)) {
+          val cycles = sofar.size - sofar.indexOf(newDistribution)
+          (step + 1, cycles)
+        }
+        else go(newDistribution, sofar ++ List(newDistribution), step + 1)
+      }
+
+      go(banks, List(List()), 0)
+    }
+  }
+
+  object Part2 {
+
+  }
+
+}
