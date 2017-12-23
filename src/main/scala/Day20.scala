7fb3d205c3a0248c78efe8196faff391aea198d4
Roland Tritsch <roland@gonito.com>
2017-12-23 09:11:46 +0000

    Day20. Part1. Working.

---
+++ src/main/scala/Day20.scala
@@ -0,0 +1,45 @@
+package aoc
+
+object Day20 {
+  val in = Util.readInput("Day20input.txt")
+
+  case class Position(x: Int, y: Int, z: Int) {
+    def add(v: Velocity): Position = {
+      Position(x + v.x, y + v.y, z + v.z)
+    }
+    def sumAbs: Int = Math.abs(x) + Math.abs(y) + Math.abs(z)
+  }
+
+  case class Velocity(x: Int, y: Int, z: Int) {
+    def add(a: Acceleration): Velocity = {
+      Velocity(x + a.x, y + a.y, z + a.z)
+    }
+  }
+  case class Acceleration(x: Int, y: Int, z: Int)
+  case class Particle(p: Position, v: Velocity, a: Acceleration) {
+    def tick: Particle = {
+      Particle(p.add(v.add(a)), v.add(a), a)
+    }
+  }
+
+  def parseInput(in: List[String]): List[Particle] = {
+    in.map(l => {
+      // p=<1199,-2918,1457>, v=<-13,115,-8>, a=<-7,8,-10>
+      val tokens = l.split("[<>,]").map(_.trim)
+      val p = Position(tokens(1).toInt, tokens(2).toInt, tokens(3).toInt)
+      val v = Velocity(tokens(6).toInt, tokens(7).toInt, tokens(8).toInt)
+      val a = Acceleration(tokens(11).toInt, tokens(12).toInt, tokens(13).toInt)
+      Particle(p, v, a)
+    })
+  }
+
+  def run(ps: List[Particle], depth: Int): List[Particle] = {
+    if(depth <= 0) ps
+    else run(ps.map(_.tick), depth - 1)
+  }
+
+  def closest(ps: List[Particle]): Int = {
+    val min = ps.map(_.p.sumAbs).min
+    ps.indexWhere(p => p.p.sumAbs == min)
+  }
+}
---
+++ src/test/scala/Day20Spec.scala
@@ -0,0 +1,48 @@
+package aoc
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Day20Spec extends FlatSpec with Matchers {
+
+  val testInput = List(
+    "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>",
+    "p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>"
+  )
+
+  val tick1Result = List(
+    "p=< 4,0,0>, v=< 1,0,0>, a=<-1,0,0>",
+    "p=< 2,0,0>, v=<-2,0,0>, a=<-2,0,0>"
+  )
+
+  val tick2Result = List(
+    "p=< 4,0,0>, v=< 0,0,0>, a=<-1,0,0>",
+    "p=<-2,0,0>, v=<-4,0,0>, a=<-2,0,0>"
+  )
+
+  val tick3Result = List(
+    "p=< 3,0,0>, v=<-1,0,0>, a=<-1,0,0>",
+    "p=<-8,0,0>, v=<-6,0,0>, a=<-2,0,0>"
+  )
+
+  "the input" should "be correct" in {
+    Day20.in.head shouldBe "p=<1199,-2918,1457>, v=<-13,115,-8>, a=<-7,8,-10>"
+  }
+
+  "the parser" should "parse the input correctly" in {
+    Day20.parseInput(testInput).head shouldBe Day20.Particle(Day20.Position(3,0,0), Day20.Velocity(2,0,0), Day20.Acceleration(-1, 0, 0))
+  }
+
+  "running the simulation" should "return the right result(s)" in {
+    Day20.run(Day20.parseInput(testInput), 1) should be (Day20.parseInput(tick1Result))
+    Day20.run(Day20.parseInput(testInput), 2) should be (Day20.parseInput(tick2Result))
+    Day20.run(Day20.parseInput(testInput), 3) should be (Day20.parseInput(tick3Result))
+  }
+
+  "closest" should "return the right result(s)" in {
+    Day20.closest(Day20.run(Day20.parseInput(testInput), 10)) shouldBe 0
+  }
+
+  it should "solve the puzzle" in {
+    Day20.closest(Day20.run(Day20.parseInput(Day20.in), 1000)) shouldBe 243
+  }
+}
