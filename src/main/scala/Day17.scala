b4e58be4e068495cd10b572c08274cf0ac812159
Roland Tritsch <roland@gonito.com>
2018-01-07 07:22:52 +0000

    Day17: Tried ListBuffer. Still to slow.

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,18 +1,11 @@
 package aoc
 
 object Day17 {
+  import scala.collection.mutable
+
   val steps = 371
   val times = 2017
   val times2 = 50000000
-
-  def buildBuffer(buffer: List[Int], steps: Int, times: Int): (Int, List[Int]) = {
-    require(steps >= 1, s"step >= 1 failed; with >${steps}<")
-    require(times >= 1, s"times >= 1 failed; with >${times}<")
-    (1 to times).foldLeft(0, buffer)((current, n) => {
-      val (currentPosition, currentBuffer) = current
-      nextBuffer(currentPosition, currentBuffer, steps, n)
-    })
-  }
 
   def moveForward(position: Int, size: Int, steps: Int): Int = {
     if (steps <= 0) position
@@ -20,27 +13,37 @@ object Day17 {
     else moveForward(position + 1, size, steps - 1)
   }
 
-  def insertAfter(position: Int, buffer: List[Int], n: Int): List[Int] = {
-    val (head, tail) = buffer.splitAt(position + 1)
-    head ++ List(n) ++ tail
+  def insertAfter(position: Int, buffer: mutable.ListBuffer[Int], n: Int): mutable.ListBuffer[Int] = {
+    buffer.insert(position + 1, n)
+    buffer
   }
 
-  def nextBuffer(position: Int, buffer: List[Int], steps: Int, n: Int): (Int, List[Int]) = {
+  def nextBuffer(position: Int, buffer: mutable.ListBuffer[Int], steps: Int, n: Int): (Int, mutable.ListBuffer[Int]) = {
     val insertPosition = moveForward(position, buffer.size, steps)
     val currentPosition = insertPosition + 1
     (currentPosition, insertAfter(insertPosition, buffer, n))
   }
 
+  def buildBuffer(buffer: mutable.ListBuffer[Int], steps: Int, times: Int): (Int, mutable.ListBuffer[Int]) = {
+    require(steps >= 1, s"step >= 1 failed; with >${steps}<")
+    require(times >= 1, s"times >= 1 failed; with >${times}<")
+    (1 to times).foldLeft(0, buffer)((current, n) => {
+      val (currentPosition, currentBuffer) = current
+      //if(currentBuffer.size % 100000 == 0) println(currentBuffer.size)
+      nextBuffer(currentPosition, currentBuffer, steps, n)
+    })
+  }
+
   object Part1 {
     def solve(steps: Int, times: Int): Int = {
-      val (position, buffer) = buildBuffer(List(0), steps, times)
+      val (position, buffer) = buildBuffer(mutable.ListBuffer(0), steps, times)
       buffer(position + 1)
     }
   }
 
   object Part2 {
     def solve(steps: Int, times: Int): Int = {
-      val (position, buffer) = buildBuffer(List(0), steps, times)
+      val (position, buffer) = buildBuffer(mutable.ListBuffer(0), steps, times)
       buffer(1)
     }
   }
--- src/test/scala/Day17Spec.scala
+++ src/test/scala/Day17Spec.scala
@@ -3,6 +3,7 @@ package aoc
 import org.scalatest.{FlatSpec, Matchers}
 
 class Day17Spec extends FlatSpec with Matchers {
+  import scala.collection.mutable
 
   val testResult = List(
     (0, List(0)),
@@ -17,21 +18,24 @@ class Day17Spec extends FlatSpec with Matchers {
     (1, List(0, 9, 5, 7, 2, 4, 3, 8, 6, 1))
   )
 
-  "moveForward" should "return the right result(s)" in {
+  behavior of "moveForward"
+  it should "return the right result(s)" in {
     Day17.moveForward(0, 1, 3) shouldBe 0
     Day17.moveForward(1, 2, 3) shouldBe 0
     Day17.moveForward(1, 3, 3) shouldBe 1
     Day17.moveForward(2, 4, 3) shouldBe 1
   }
 
-  "insertAfter" should "return the right result(s)" in {
-    Day17.insertAfter(0, List(0), 1) should be (List(0, 1))
-    Day17.insertAfter(0, List(0, 1), 2) should be (List(0, 2, 1))
-    Day17.insertAfter(1, List(0, 2, 1), 3) should be (List(0, 2, 3, 1))
+  behavior of "insertAfter"
+  it should "return the right result(s)" in {
+    Day17.insertAfter(0, mutable.ListBuffer(0), 1) should be (List(0, 1))
+    Day17.insertAfter(0, mutable.ListBuffer(0, 1), 2) should be (List(0, 2, 1))
+    Day17.insertAfter(1, mutable.ListBuffer(0, 2, 1), 3) should be (List(0, 2, 3, 1))
   }
 
-  "nextBuffer" should "return the next buffer" in {
-    val step1 = Day17.nextBuffer(0, List(0), 3, 1)
+  behavior of "nextBuffer"
+  it should "return the next buffer" in {
+    val step1 = Day17.nextBuffer(0, mutable.ListBuffer(0), 3, 1)
     step1 should be (1, List(0, 1))
 
     val step2 = Day17.nextBuffer(step1._1, step1._2, 3, 2)
@@ -41,25 +45,26 @@ class Day17Spec extends FlatSpec with Matchers {
     step3 should be (2, List(0, 2, 3, 1))
   }
 
-  "buildBuffer" should "return the right result(s)" in {
-    Day17.buildBuffer(List(0), 3, 1) should be (1, List(0, 1))
-    Day17.buildBuffer(List(0), 3, 2) should be (1, List(0, 2, 1))
-    Day17.buildBuffer(List(0), 3, 3) should be (2, List(0, 2, 3, 1))
+  behavior of "buildBuffer"
+  it should "return the right result(s)" in {
+    Day17.buildBuffer(mutable.ListBuffer(0), 3, 1) should be (1, List(0, 1))
+    Day17.buildBuffer(mutable.ListBuffer(0), 3, 2) should be (1, List(0, 2, 1))
+    Day17.buildBuffer(mutable.ListBuffer(0), 3, 3) should be (2, List(0, 2, 3, 1))
 
-    val (position, buffer) = Day17.buildBuffer(List(0), 3, Day17.times)
+    val (position, buffer) = Day17.buildBuffer(mutable.ListBuffer(0), 3, Day17.times)
     buffer(position) shouldBe Day17.times
     buffer.slice(position - 3, position + 3 + 1) should be (List(1512, 1134, 151, 2017, 638, 1513, 851))
     buffer(position + 1) shouldBe 638
   }
 
   it should "solve the puzzle" in {
-    val (position, buffer) = Day17.buildBuffer(List(0), Day17.steps, Day17.times)
+    val (position, buffer) = Day17.buildBuffer(mutable.ListBuffer(0), Day17.steps, Day17.times)
     buffer(position) shouldBe Day17.times
     buffer(position + 1) shouldBe 1311
   }
 
   ignore should "solve the puzzle (Part2)" in {
-    val (position, buffer) = Day17.buildBuffer(List(0), Day17.steps, Day17.times2)
+    val (position, buffer) = Day17.buildBuffer(mutable.ListBuffer(0), Day17.steps, Day17.times2)
     buffer(position) shouldBe Day17.times2
     buffer(1) shouldBe 0
   }
