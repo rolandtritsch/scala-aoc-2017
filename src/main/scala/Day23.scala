0d263ea06d32217900cf750240d3c8a5a645e0bf
Roland Tritsch <roland@gonito.com>
2018-01-10 18:01:43 +0000

    Day23: Part1: Done. Working.

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -4,11 +4,11 @@ object Day23 {
   val in = Util.readInput("Day23input.txt")
 
   sealed abstract class Operation
-  case class Set(register: Char, value: Int) extends Operation
+  case class Set(register: Char, value: Long) extends Operation
   case class SetR(register: Char, value: Char) extends Operation
-  case class Sub(register: Char, value: Int) extends Operation
+  case class Sub(register: Char, value: Long) extends Operation
   case class SubR(register: Char, value: Char) extends Operation
-  case class Mul(register: Char, value: Int) extends Operation
+  case class Mul(register: Char, value: Long) extends Operation
   case class MulR(register: Char, value: Char) extends Operation
   case class JumpIfNotZero(register: Char, value: Int) extends Operation
 
@@ -25,19 +25,19 @@ object Day23 {
           assert(tokens.size == 3)
           val operand = tokens(2)
           if(registerRange.contains(operand.charAt(0))) SetR(register, operand.charAt(0))
-          else Set(register, operand.toInt)
+          else Set(register, operand.toLong)
         }
         case "sub" => {
           assert(tokens.size == 3)
           val operand = tokens(2)
           if(registerRange.contains(operand.charAt(0))) SubR(register, operand.charAt(0))
-          else Sub(register, operand.toInt)
+          else Sub(register, operand.toLong)
         }
         case "mul" => {
           assert(tokens.size == 3)
           val operand = tokens(2)
           if(registerRange.contains(operand.charAt(0))) MulR(register, operand.charAt(0))
-          else Mul(register, operand.toInt)
+          else Mul(register, operand.toLong)
         }
         case "jnz" => {
           assert(tokens.size == 3)
@@ -52,51 +52,102 @@ object Day23 {
     })
   }
 
-  case class Program(counter: Int, instructions: List[Operation], register: Map[Char, Int], instructionCounter: Map[String, Int])
+  case class Program(counter: Int, instructions: List[Operation], register: Map[Char, Long], instructionCounter: Map[String, Long])
 
-  def run(program: Program, done: Program => Boolean, exit: Program => Int): Int = {
+  def run(program: Program, done: Program => Boolean, exit: Program => Long): Long = {
     //println(s"${program.counter}/${program.instructions(program.counter)}/${program.register.toList}")
+    //println(s"${program.counter}/${program.instructions(program.counter)}/${program.instructionCounter("mul")}")
     if(done(program)) exit(program)
     else {
       val next = program.instructions(program.counter) match {
         case Set(r, v) => {
-          Program(program.counter + 1, program.instructions, program.register + (r -> v), program.instructionCounter + ("set" -> (program.instructionCounter("set") + 1)))
+          Program(
+            program.counter + 1,
+            program.instructions,
+            program.register + (r -> v),
+            program.instructionCounter + ("set" -> (program.instructionCounter("set") + 1))
+          )
         }
         case SetR(r, v) => {
-          Program(program.counter + 1, program.instructions, program.register + (r -> program.register(v)), program.instructionCounter + ("set" -> (program.instructionCounter("set") + 1)))
+          Program(
+            program.counter + 1,
+            program.instructions,
+            program.register + (r -> program.register(v)),
+            program.instructionCounter + ("set" -> (program.instructionCounter("set") + 1))
+          )
         }
         case Sub(r, v) => {
-          Program(program.counter + 1, program.instructions, program.register + (r -> (program.register(r) - v)), program.instructionCounter + ("sub" -> (program.instructionCounter("sub") + 1)))
+          Program(
+            program.counter + 1,
+            program.instructions,
+            program.register + (r -> (program.register(r) - v)),
+            program.instructionCounter + ("sub" -> (program.instructionCounter("sub") + 1))
+          )
         }
         case SubR(r, v) => {
-          Program(program.counter + 1, program.instructions, program.register + (r -> (program.register(r) - program.register(v))), program.instructionCounter + ("sub" -> (program.instructionCounter("sub") + 1)))
+          Program(
+            program.counter + 1,
+            program.instructions,
+            program.register + (r -> (program.register(r) - program.register(v))),
+            program.instructionCounter + ("sub" -> (program.instructionCounter("sub") + 1))
+          )
         }
         case Mul(r, v) => {
-          Program(program.counter + 1, program.instructions, program.register + (r -> (program.register(r) * v)), program.instructionCounter + ("mul" -> (program.instructionCounter("mul") + 1)))
+          Program(
+            program.counter + 1,
+            program.instructions,
+            program.register + (r -> (program.register(r) * v)),
+            program.instructionCounter + ("mul" -> (program.instructionCounter("mul") + 1))
+          )
         }
         case MulR(r, v) => {
-          Program(program.counter + 1, program.instructions, program.register + (r -> (program.register(r) * program.register(v))), program.instructionCounter + ("mul" -> (program.instructionCounter("mul") + 1)))
+          Program(
+            program.counter + 1,
+            program.instructions,
+            program.register + (r -> (program.register(r) * program.register(v))),
+            program.instructionCounter + ("mul" -> (program.instructionCounter("mul") + 1))
+          )
         }
         case JumpIfNotZero(r, v) => {
-          if(program.register(r) != 0) Program(program.counter + v, program.instructions, program.register, program.instructionCounter + ("jnz" -> (program.instructionCounter("jnz") + 1)))
-          else Program(program.counter + 1, program.instructions, program.register, program.instructionCounter + ("jnz" -> (program.instructionCounter("jnz") + 1)))
+          if(program.register(r) != 0) {
+            Program(
+              program.counter + v,
+              program.instructions,
+              program.register,
+              program.instructionCounter + ("jnz" -> (program.instructionCounter("jnz") + 1))
+            )
+          }
+          else {
+            Program(
+              program.counter + 1,
+              program.instructions,
+              program.register,
+              program.instructionCounter + ("jnz" -> (program.instructionCounter("jnz") + 1))
+            )
+          }
         }
       }
       run(next, done, exit)
     }
   }
 
-  def fullRun(program: Program): Int = {
+  def fullRun(program: Program): Long = {
     def done(p: Program) = p.counter < 0 || p.counter >= p.instructions.size
     def exit(p: Program) = -1
 
     run(program, done, exit)
   }
 
-  def solveRun(program: Program): Int = {
+  def solveRun(program: Program): Long = {
     def done(p: Program) = p.counter < 0 || p.counter >= p.instructions.size
-    //def done(p: Program) = p.counter < 0 || p.counter >= 19
-    def exit(p: Program) = program.instructionCounter("mul")
+    def exit(p: Program) = p.instructionCounter("mul")
+
+    run(program, done, exit)
+  }
+
+  def solveRun2(program: Program): Long = {
+    def done(p: Program) = p.counter < 0 || p.counter >= p.instructions.size
+    def exit(p: Program) = p.register('h')
 
     run(program, done, exit)
   }
--- src/test/scala/Day23Spec.scala
+++ src/test/scala/Day23Spec.scala
@@ -4,14 +4,25 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Day23Spec extends FlatSpec with Matchers {
   "the input" should "be correct" in {
-    Day23.in.head shouldBe "set b 84"
+    Day23.in.head shouldBe "set l 1"
   }
 
   "the parser" should "return the correct result(s)" in {
-    Day23.parseInput(Day23.in).size shouldBe 32
+    Day23.parseInput(Day23.in).size shouldBe 33
   }
 
   "running the program" should "solve the puzzle" in {
-    //Day23.solveRun(Day23.Program(0, Day23.parseInput(Day23.in), Map.empty[Char, Int].withDefaultValue(0), Map.empty[String, Int].withDefaultValue(0))) shouldBe 0
+    Day23.solveRun(Day23.Program(0, Day23.parseInput(Day23.in), Map.empty[Char, Long].withDefaultValue(0), Map.empty[String, Long].withDefaultValue(0))) shouldBe 6724
+  }
+
+  "running the program" should "solve the puzzle (again)" in {
+    Day23.solveRun2(
+      Day23.Program(
+        0,
+        Day23.parseInput(Day23.in),
+        Map.empty[Char, Long].withDefaultValue(0L) + ('a' -> 1L),
+        Map.empty[String, Long].withDefaultValue(0)
+      )
+    ) shouldBe 0
   }
 }
