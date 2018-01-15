e18a1bb3e5757546f81778f3e27cc1dc86673eed
Roland Tritsch <roland@gonito.com>
2018-01-15 06:00:51 +0000

    Day11: Part1/2: Done. Working.

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -1,9 +1,30 @@
 package aoc
 
+// @note https://stackoverflow.com/a/47749887/2374327
 object Day11 {
   val in = Util.readInput("Day11input.txt").head.split(',').toList
 
-  def calcSteps(path: List[String]): Int = {
-    0
+  case class Tile(x: Int, y: Int, z: Int) {
+    def distance(that: Tile): Int = {
+      (Math.abs(this.x - that.x) + Math.abs(this.y - that.y) + Math.abs(this.z - that.z)) / 2
+    }
+  }
+
+  def calcSteps(path: List[String]): (Int, Int) = {
+    val centerTile = Tile(0, 0, 0)
+    val (finalTile, finalMax) = path.foldLeft(centerTile, 0)((current, move) => {
+      val (currentTile, currentMax) = current
+      val nextTile = move match {
+        case "n" => Tile(currentTile.x, currentTile.y + 1, currentTile.z - 1)
+        case "ne" => Tile(currentTile.x + 1, currentTile.y, currentTile.z - 1)
+        case "nw" => Tile(currentTile.x - 1 , currentTile.y + 1, currentTile.z)
+        case "s" => Tile(currentTile.x, currentTile.y - 1, currentTile.z + 1)
+        case "se" => Tile(currentTile.x + 1, currentTile.y - 1, currentTile.z)
+        case "sw" => Tile(currentTile.x - 1, currentTile.y, currentTile.z + 1)
+        case _ => {assert(false); Tile(0, 0, 0)}
+      }
+      (nextTile, Math.max(currentMax, centerTile.distance(nextTile)))
+    })
+    (centerTile.distance(finalTile), finalMax)
   }
 }
--- src/test/scala/Day11Spec.scala
+++ src/test/scala/Day11Spec.scala
@@ -4,15 +4,20 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Day11Spec extends FlatSpec with Matchers {
 
-  "input" should "be correct" in {
+  behavior of "input"
+  it should "be correct" in {
     Day11.in.take(5) should be (List("nw", "n", "n", "se", "ne"))
   }
 
-  "calcSteps" should "return the correct results" in {
-    Day11.calcSteps(List("ne", "ne", "ne")) shouldBe 0
-    Day11.calcSteps(List("ne", "ne", "sw", "sw")) shouldBe 0
-    Day11.calcSteps(List("ne", "ne", "s", "s")) shouldBe 0
-    Day11.calcSteps(List("se", "sw", "se", "sw", "sw")) shouldBe 0
+  behavior of "calcSteps"
+  it should "return the correct results" in {
+    Day11.calcSteps(List("ne", "ne", "ne")) shouldBe (3, 3)
+    Day11.calcSteps(List("ne", "ne", "sw", "sw")) shouldBe (0, 2)
+    Day11.calcSteps(List("ne", "ne", "s", "s")) shouldBe (2, 2)
+    Day11.calcSteps(List("se", "sw", "se", "sw", "sw")) shouldBe (3, 3)
   }
 
+  it should "solve the puzzle" in {
+    Day11.calcSteps(Day11.in) shouldBe (810, 1567)
+  }
 }
