f43cf61a5413357c5bff04d04a8aae300be8ceba
Roland Tritsch <roland@gonito.com>
2018-01-15 06:05:10 +0000

    Day23: Added behavior

--- src/test/scala/Day23Spec.scala
+++ src/test/scala/Day23Spec.scala
@@ -3,19 +3,22 @@ package aoc
 import org.scalatest.{FlatSpec, Matchers}
 
 class Day23Spec extends FlatSpec with Matchers {
-  "the input" should "be correct" in {
+  behavior of "the input"
+  it should "be correct" in {
     Day23.in.head shouldBe "set l 1"
   }
 
-  "the parser" should "return the correct result(s)" in {
+  behavior of "the parser"
+  it should "return the correct result(s)" in {
     Day23.parseInput(Day23.in).size shouldBe 33
   }
 
-  "running the program" should "solve the puzzle" in {
+  behavior of "running the program"
+  it should "solve the puzzle" in {
     Day23.solveRun(Day23.Program(0, Day23.parseInput(Day23.in), Map.empty[Char, Long].withDefaultValue(0), Map.empty[String, Long].withDefaultValue(0))) shouldBe 6724
   }
 
-  "running the program" should "solve the puzzle (again)" in {
+  ignore should "solve the puzzle (again)" in {
     Day23.solveRun2(
       Day23.Program(
         0,
