286761908508f292bdbd34ea7f6f9085fc2d3bfe
Roland Tritsch <roland@gonito.com>
2018-02-19 14:19:01 +0000

    Day5: Refactor. Added Meter test(s). Fixed the performance.

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -11,6 +11,8 @@ package aoc
   * Note: The given program will exit at the higher end of the stack, but
   * for completeness we are also checking for the lower bound.
   *
+  * Note: Picking the right [[https://docs.scala-lang.org/overviews/collections/performance-characteristics.html datastructure]] is important.
+  *
   * Part1 - Trival. Just increase the stack counter by 1.
   *
   * Part2 - Increase the stack counter as described in the problem statement.
@@ -19,8 +21,8 @@ object Day05 {
 
   val input = Util.readInput("Day05input.txt").map(_.toInt)
 
-  def countSteps(stack: List[Int], stackCounter: Int, steps: Int, offset: Int => Int): Int = {
-    def outOfBounds(stack: List[Int], stackCounter: Int) = stackCounter + stack(stackCounter) < 0 || stackCounter + stack(stackCounter) >= stack.size
+  def countSteps(stack: Vector[Int], stackCounter: Int, steps: Int, offset: Int => Int): Int = {
+    def outOfBounds(stack: Vector[Int], stackCounter: Int) = stackCounter + stack(stackCounter) < 0 || stackCounter + stack(stackCounter) >= stack.size
 
     if(outOfBounds(stack, stackCounter)) steps
     else countSteps(stack.updated(stackCounter, offset(stack(stackCounter))), stackCounter + stack(stackCounter), steps + 1, offset)
@@ -28,11 +30,11 @@ object Day05 {
 
   object Part1 {
     def offset(sc: Int) = sc + 1
-    def solve(stack: List[Int]): Int = countSteps(stack, 0, 1, offset)
+    def solve(stack: List[Int]): Int = countSteps(stack.toVector, 0, 1, offset)
   }
 
   object Part2 {
     def offset(sc: Int) = if(sc >= 3) sc - 1 else sc + 1
-    def solve(stack: List[Int]): Int = countSteps(stack, 0, 1, offset)
+    def solve(stack: List[Int]): Int = countSteps(stack.toVector, 0, 1, offset)
   }
 }
