a1819ee7e7accebb1c37eb37c8bb588a0bacd107
Roland Tritsch <roland@gonito.com>
2018-02-19 13:36:22 +0000

    Day5: Refactor. Simplify. Improve readability. Part2 (still) very slow.

--- src/main/scala/Day05.scala
+++ src/main/scala/Day05.scala
@@ -1,27 +1,38 @@
 package aoc
 
+/** Problem: [[https://adventofcode.com/2017/day/5]]
+  *
+  * Solution:
+  *
+  * General - Walk through the stack and update the stack with the offset()
+  * function until the stack counter is/runs out of bounds (which indicates
+  * that the "program" has exited.
+  *
+  * Note: The given program will exit at the higher end of the stack, but
+  * for completeness we are also checking for the lower bound.
+  *
+  * Part1 - Trival. Just increase the stack counter by 1.
+  *
+  * Part2 - Increase the stack counter as described in the problem statement.
+  */
 object Day05 {
 
-  val in = Util.readInput("Day05input.txt").map(_.toInt)
+  val input = Util.readInput("Day05input.txt").map(_.toInt)
+
+  def countSteps(stack: List[Int], stackCounter: Int, steps: Int, offset: Int => Int): Int = {
+    def outOfBounds(stack: List[Int], stackCounter: Int) = stackCounter + stack(stackCounter) < 0 || stackCounter + stack(stackCounter) >= stack.size
+
+    if(outOfBounds(stack, stackCounter)) steps
+    else countSteps(stack.updated(stackCounter, offset(stack(stackCounter))), stackCounter + stack(stackCounter), steps + 1, offset)
+  }
 
   object Part1 {
-    def countSteps(stack: List[Int], stackCounter: Int, steps: Int = 1): Int = {
-      if(stackCounter + stack(stackCounter) >= stack.size) steps
-      else countSteps(stack.updated(stackCounter, stack(stackCounter) + 1), stackCounter + stack(stackCounter), steps + 1)
-    }
+    def offset(sc: Int) = sc + 1
+    def solve(stack: List[Int]): Int = countSteps(stack, 0, 1, offset)
   }
 
   object Part2 {
-    def countSteps(stack: List[Int], stackCounter: Int, steps: Int = 1): (Int, List[Int]) = {
-      if(stackCounter + stack(stackCounter) >= stack.size) (steps, stack)
-      else countSteps(
-        stack.updated(
-          stackCounter,
-          if(stack(stackCounter) >= 3) stack(stackCounter) - 1 else stack(stackCounter) + 1
-        ),
-        stackCounter + stack(stackCounter),
-        steps + 1
-      )
-    }
+    def offset(sc: Int) = if(sc >= 3) sc - 1 else sc + 1
+    def solve(stack: List[Int]): Int = countSteps(stack, 0, 1, offset)
   }
 }
--- src/test/scala/Day05Spec.scala
+++ src/test/scala/Day05Spec.scala
@@ -4,28 +4,28 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Day05Spec extends FlatSpec with Matchers {
 
-  behavior of "readInput"
+  behavior of "readInput()"
   it should "read the instructions" in {
-    Day05.in.take(10) should be (List(0, 1, 0, 0, 1, -3, 0, 0, 2, -2))
+    Day05.input.take(10) should be (List(0, 1, 0, 0, 1, -3, 0, 0, 2, -2))
   }
 
   val testStack = List(0, 3, 0, 1, -3)
 
-  behavior of "Part 1 - countSteps"
-  it should "return the right result(s)" in {
-    //Day05.Part1.countSteps(testStack, 0) shouldBe 5
+  behavior of "solve() - Part1"
+  it should "solve the testcase(s)" in {
+    Day05.Part1.solve(testStack) shouldBe 5
   }
 
-  ignore should "solve the puzzle" in {
-    Day05.Part1.countSteps(Day05.in, 0) shouldBe 372139
+  it should "solve the puzzle" taggedAs(SolutionTest) in {
+    Day05.Part1.solve(Day05.input) shouldBe 372139
   }
 
-  behavior of "Part 2 - countSteps"
-  it should "return the right result(s)" in {
-    //Day05.Part2.countSteps(testStack, 0) shouldBe (10, List(2, 3, 2, 3, -1))
+  behavior of "solve() - Part2"
+  it should "solve the testcase(s)" in {
+    Day05.Part2.solve(testStack) shouldBe 10
   }
 
-  ignore should "solve the puzzle" in {
-    Day05.Part2.countSteps(Day05.in, 0)._1 shouldBe 29629538
+  it should "solve the puzzle" taggedAs(SolutionTest, SlowTest) in {
+    Day05.Part2.solve(Day05.input) shouldBe 29629538
   }
 }
