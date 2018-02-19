4a7bfa97b131aba8a4974d428d32d7c00c19ec26
Roland Tritsch <roland@gonito.com>
2018-02-19 22:23:19 +0000

    Day6: Refactor. Simplify. Improve readability. Using floyd.

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -1,34 +1,69 @@
 package aoc
 
+/** Problem: [[https://adventofcode.com/2017/day/6]]
+  *
+  * General - This is a [[https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_Tortoise_and_Hare cycle detection problem]].
+  *
+  * Part1 - Is lambda + mu.
+  *
+  * Part2 - Is just lambda.
+  */
 object Day06 {
 
-  val in = Util.readInput("Day06input.txt").head.split('\t').map(_.toInt).toList
+  val input = Util.readInput("Day06input.txt").head.split('\t').map(_.toInt).toList
 
-  object Part1 {
-    def redistribute(banks: List[Int]): List[Int] = {
-      val maxBlocks = banks.max
-      val mostBlocksIndex = banks.indexWhere(b => b == maxBlocks)
-      var newBanks = banks.updated(mostBlocksIndex, 0)
-      for {
-        i <- 1 to maxBlocks
-        n = ((mostBlocksIndex + i) % banks.size)
-      } {
-        newBanks = newBanks.updated(n, newBanks(n) + 1)
-      }
-      newBanks
+  type MemoryBanks = List[Int]
+
+  def cycle(banks: MemoryBanks): MemoryBanks = {
+    val maxBlocks = banks.max
+    val mostBlocksIndex = banks.indexWhere(b => b == maxBlocks)
+    var newBanks = banks.updated(mostBlocksIndex, 0)
+    for {
+      i <- 1 to maxBlocks
+      n = ((mostBlocksIndex + i) % banks.size)
+    } {
+      newBanks = newBanks.updated(n, newBanks(n) + 1)
+    }
+    newBanks
+  }
+
+  def floyd(banks: MemoryBanks, cycle: MemoryBanks => MemoryBanks): (Int, Int) = {
+    var tortoise = cycle(banks)
+    var hare = cycle(cycle(banks))
+    while(tortoise != hare) {
+      tortoise = cycle(tortoise)
+      hare = cycle(cycle(hare))
     }
 
-    def detectLoop(banks: List[Int]): (Int, Int) = {
-      def go(distribution: List[Int], sofar: List[List[Int]], step: Int): (Int, Int) = {
-        val newDistribution = redistribute(distribution)
-        if (sofar.contains(newDistribution)) {
-          val cycles = sofar.size - sofar.indexOf(newDistribution)
-          (step + 1, cycles)
-        }
-        else go(newDistribution, sofar ++ List(newDistribution), step + 1)
-      }
+    var mu = 0
+    tortoise = banks
+    while(tortoise != hare) {
+      tortoise = cycle(tortoise)
+      hare = cycle(hare)
+      mu = mu + 1
+    }
 
-      go(banks, List(List()), 0)
+    var lambda = 1
+    hare = cycle(tortoise)
+    while(tortoise != hare) {
+      hare = cycle(hare)
+      lambda = lambda + 1
+    }
+
+    return(lambda, mu)
+  }
+
+  object Part1 {
+    def solve(banks: MemoryBanks): Int = {
+      val (lambda, mu) = floyd(banks, cycle)
+      lambda + mu
+    }
+  }
+
+  object Part2 {
+    def solve(banks: MemoryBanks): Int = {
+      val (lambda, mu) = floyd(banks, cycle)
+      lambda
     }
   }
 }
--- src/test/scala/Day06Spec.scala
+++ src/test/scala/Day06Spec.scala
@@ -5,35 +5,43 @@ import org.scalatest.{FlatSpec, Matchers}
 class Day06Spec extends FlatSpec with Matchers {
   val testBanks = List(0, 2, 7, 0)
 
-  behavior of "readInput"
+  behavior of "readInput()"
   it should "read the input" in {
-    Day06.in should be (List(10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6))
+    Day06.input should be (List(10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6))
   }
   behavior of "redistribute"
   it should "return the correct results" in {
-    val step1 = Day06.Part1.redistribute(testBanks)
+    val step1 = Day06.cycle(testBanks)
     step1 should be (List(2, 4, 1, 2))
 
-    val step2 = Day06.Part1.redistribute(step1)
+    val step2 = Day06.cycle(step1)
     step2 should be (List(3, 1, 2, 3))
 
-    val step3 = Day06.Part1.redistribute(step2)
+    val step3 = Day06.cycle(step2)
     step3 should be (List(0, 2, 3, 4))
 
-    val step4 = Day06.Part1.redistribute(step3)
+    val step4 = Day06.cycle(step3)
     step4 should be (List(1, 3, 4, 1))
 
-    val step5 = Day06.Part1.redistribute(step4)
+    val step5 = Day06.cycle(step4)
     step5 should be (step1)
   }
 
-  behavior of "detectLoop"
-  it should "return the correct result(s)" in {
-    Day06.Part1.detectLoop(testBanks) shouldBe (5, 4)
+  behavior of "solve() - Part1"
+  it should "solve the testcase" in {
+    Day06.Part1.solve(testBanks) shouldBe 5
   }
 
   it should "solve the puzzle" in {
-    Day06.Part1.detectLoop(Day06.in) shouldBe (14029, 2765)
+    Day06.Part1.solve(Day06.input) shouldBe 14029
+  }
+
+  behavior of "solve() - Part2"
+  it should "solve the testcase" in {
+    Day06.Part2.solve(testBanks) shouldBe 4
+  }
+
+  it should "solve the puzzle" in {
+    Day06.Part2.solve(Day06.input) shouldBe 2765
   }
 }
-
