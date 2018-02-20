7f1813749930661617e0a7294871f851ba9e83e4
Roland Tritsch <roland@gonito.com>
2018-02-20 15:32:09 +0000

    Day8: Refactor. Simplify. Improve readability.

--- src/main/scala/Day08.scala
+++ src/main/scala/Day08.scala
@@ -1,8 +1,22 @@
 package aoc
 
+/** Problem: [[https://adventofcode.com/2017/day/8]]
+  *
+  * Solution:
+  *
+  * General - Very simple idea. We have a set of (immutable) instructions (no jumps)
+  * and a set of registers. We just need to go through all instructions and update
+  * the set of registers on the way (with every instruction). While we do this, we
+  * collect all register states. The final register state is the head of the collected
+  * register states.
+  *
+  * Part1 - Simple. The max of the register values in the final/last register state/set.
+  *
+  * Part2 - Simple. The max of all register values in all collected register states/sets.
+  */
 object Day08 {
 
-  val in = Util.readInput("Day08input.txt")
+  val input = Util.readInput("Day08input.txt")
 
   case class Instruction(register: String, operation: String, operand: Int, conditionRegister: String, condition: String, conditionOperand: Int)
 
@@ -31,11 +45,13 @@ object Day08 {
   }
 
   def buildRegisters(instructions: List[Instruction]): Map[String, Int] = {
+    require(instructions.nonEmpty, s"instructions.nonEmpty failed")
     instructions.map(i => (i.register, 0)).toMap
-  }
+  } ensuring(_.nonEmpty, s"_.nonEmpty failed")
 
   def evalCondition(i: Instruction, registers: Map[String, Int]): Boolean = {
-    //println(s"${i.conditionRegister}(${registers(i.conditionRegister)})/${i.condition}/${i.conditionOperand}")
+    require(List("==", "!=", "<", ">", "<=", ">=").contains(i.condition), s"List(conditions).contains(i.condition) failed; with >${i.condition}<")
+
     i.condition match {
       case "==" if(registers(i.conditionRegister) == i.conditionOperand) => true
       case "!=" if(registers(i.conditionRegister) != i.conditionOperand) => true
@@ -48,11 +64,13 @@ object Day08 {
   }
 
   def executeInstruction(i: Instruction, registers: Map[String, Int]): Map[String, Int] = {
+    require(List("inc", "dec").contains(i.operation), s"List(operations).contains(i.operation) failed; with >${i.operation}<")
+
     if(evalCondition(i, registers)) {
-      //println(s"${i.operation}/${i.operand}/${i.register}/${registers(i.register)}")
       i.operation match {
         case "inc" => registers + (i.register -> (registers(i.register) + i.operand))
         case "dec" => registers + (i.register -> (registers(i.register) - i.operand))
+        case _ => assert(false); Map.empty[String, Int]
       }
     } else {
       registers
@@ -60,31 +78,32 @@ object Day08 {
   }
 
   def runProgram(instructions: List[Instruction], registers: Map[String, Int]): List[Map[String, Int]] = {
-    instructions.foldLeft(List(registers))((acc, i) => {
-      val newRegisters = executeInstruction(i, acc.head)
-      newRegisters :: acc
+    require(instructions.nonEmpty, s"instructions.nonEmpty")
+    require(registers.nonEmpty, s"registers.nonEmpty")
+
+    instructions.foldLeft(List(registers))((currentRegisters, i) => {
+      val newRegisters = executeInstruction(i, currentRegisters.head)
+      newRegisters :: currentRegisters
     })
   }
 
-  def maxRegister(registers: Map[String, Int]): Int = {
-    registers.values.max
+  object Part1 {
+    def solve(input: List[String]): Int = {
+      val instructions = parseInput(input)
+      val registers = buildRegisters(instructions)
+      val allRegisterStates = runProgram(instructions, registers)
+
+      allRegisterStates.head.values.max
+    }
   }
 
-  def maxStack(stack: List[Map[String, Int]]): Int = {
-    stack.flatMap(_.values).max
-  }
+  object Part2 {
+    def solve(input: List[String]): Int = {
+      val instructions = parseInput(input)
+      val registers = buildRegisters(instructions)
+      val allRegisterStates = runProgram(instructions, registers)
 
-  def findMaxRegister(in: List[String]): Int = {
-    val instructions = parseInput(in)
-    val registers = buildRegisters(instructions)
-    val stack = runProgram(instructions, registers)
-    maxRegister(stack.head)
-  }
-
-  def findMaxStack(in: List[String]): Int = {
-    val instructions = parseInput(in)
-    val registers = buildRegisters(instructions)
-    val stack = runProgram(instructions, registers)
-    maxStack(stack)
+      allRegisterStates.flatMap(_.values).max
+    }
   }
 }
--- src/test/scala/Day08Spec.scala
+++ src/test/scala/Day08Spec.scala
@@ -18,17 +18,17 @@ class Day08Spec extends FlatSpec with Matchers {
     Day08.Instruction("c", "inc", -20, "c", "==", 10)
   )
 
-  behavior of "readInput"
+  behavior of "readInput()"
   it should "read the input" in {
-    Day08.in.head shouldBe "g dec 231 if bfx > -10"
+    Day08.input.head shouldBe "g dec 231 if bfx > -10"
   }
 
-  behavior of "parseInput"
+  behavior of "parseInput()"
   it should "return the list of instructions" in {
     Day08.parseInput(testInput) should be (testInstructions)
   }
 
-  behavior of "runProgram"
+  behavior of "runProgram()"
   it should "return the stack of registers" in {
     val instructions = Day08.parseInput(testInput)
     val registers = Day08.buildRegisters(instructions)
@@ -36,33 +36,21 @@ class Day08Spec extends FlatSpec with Matchers {
     stack.head.toList.sorted should be (List(("a", 1), ("b", 0), ("c", -10)))
   }
 
-  behavior of "maxRegister"
-  it should "return the max of all registers" in {
-    val instructions = Day08.parseInput(testInput)
-    val registers = Day08.buildRegisters(instructions)
-    val stack = Day08.runProgram(instructions, registers)
-    Day08.maxRegister(stack.head) shouldBe 1
+  behavior of "solve() - Part1"
+  it should "solve the testcase(s)" in {
+    Day08.Part1.solve(testInput) shouldBe 1
   }
 
-  it should "solve the puzzle" in {
-    val instructions = Day08.parseInput(Day08.in)
-    val registers = Day08.buildRegisters(instructions)
-    val stack = Day08.runProgram(instructions, registers)
-    Day08.maxRegister(stack.head) shouldBe 4163
+  it should "solve the puzzle" taggedAs(SolutionTest) in {
+    Day08.Part1.solve(Day08.input) shouldBe 4163
   }
 
-  behavior of "maxStack"
-  it should "return the max of all register values in the stack" in {
-    val instructions = Day08.parseInput(testInput)
-    val registers = Day08.buildRegisters(instructions)
-    val stack = Day08.runProgram(instructions, registers)
-    Day08.maxStack(stack) shouldBe 10
+  behavior of "solve() - Part2"
+  it should "solve the testcase(s)" in {
+    Day08.Part2.solve(testInput) shouldBe 10
   }
 
-  it should "should solve the puzzle" in {
-    val instructions = Day08.parseInput(Day08.in)
-    val registers = Day08.buildRegisters(instructions)
-    val stack = Day08.runProgram(instructions, registers)
-    Day08.maxStack(stack) shouldBe 5347
+  it should "should solve the puzzle" taggedAs(SolutionTest) in {
+    Day08.Part2.solve(Day08.input) shouldBe 5347
   }
 }
