44ab385ff63b48b4873bdf4baae1aa73d469c8f3
Roland Tritsch <roland@gonito.com>
2018-02-21 14:02:03 +0000

    Day9: Refactor. Cleanup/delete old (recursive) implementation.

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -1,38 +1,26 @@
 package aoc
 
+/** Problem: [[https://adventofcode.com/2017/day/9]]
+  *
+  * Solution:
+  *
+  * General - My first implementation was based on a recursive processing
+  * of the input stream. Hard to read. Hard to understand. Hard to extend/
+  * maintain. And hard to run (needs a special -Xss setting (more/large
+  * stack).
+  *
+  * This (refactored) implementation is using a state machine. The states
+  * are OutOfGroup, InGroup, InGarbage, InCanceled ([[https://www.dropbox.com/s/pwogl9jhc8x7rqe/2018-02-21%2013.01.24.jpg?dl=0 pic]]). While I am running the
+  * state machine I am collecting stats that will allow me (at the end) to
+  * answer (the given) questions about the input stream.
+  *
+  * Part1 - Trivial. Collect and show the right stats.
+  *
+  * Part2 - Trivial. Collect and show the right stats.
+  */
 object Day09 {
 
   val input = Util.readInput("Day09input.txt").head.toList
-
-  def score(stream: List[Char]): (List[Int], Int) = {
-    def goGroup(stream: List[Char], level: Int, scores: List[Int], garbageCounter: Int): (List[Int], Int) = stream match {
-      case Nil => assert(false); (List(), 0)
-      case h :: Nil => assert(h == '}'); (scores, garbageCounter)
-      case h :: rest if(h == '{') => goGroup(rest, level + 1, scores, garbageCounter)
-      case h :: rest if(h == '}') => goGroup(rest, level - 1, level :: scores, garbageCounter)
-      case h :: rest if(h == '<') => goGarbage(rest, level, scores, garbageCounter)
-      case h :: rest => goGroup(rest, level, scores, garbageCounter)
-      case _ => assert(false); (List(), 0)
-    }
-
-    def goGarbage(stream: List[Char], level: Int, scores: List[Int], garbageCounter: Int): (List[Int], Int) = stream match {
-      case Nil => assert(false); (List(), 0)
-      case h :: Nil => assert(false); (List(), 0)
-      case h :: rest if(h == '!') => goCanceled(rest, level, scores, garbageCounter)
-      case h :: rest if(h == '>') => goGroup(rest, level, scores, garbageCounter)
-      case h :: rest => goGarbage(rest, level, scores, garbageCounter + 1)
-      case _ => assert(false); (List(), 0)
-    }
-
-    def goCanceled(stream: List[Char], level: Int, scores: List[Int], garbageCounter: Int): (List[Int], Int) = stream match {
-      case Nil => assert(false); (List(), 0)
-      case h :: Nil => assert(false); (List(), 0)
-      case h :: rest => goGarbage(rest, level, scores, garbageCounter)
-      case _ => assert(false); (List(), 0)
-    }
-
-    goGroup(stream, 0, List(1), 0)
-  }
 
   case class Statistics(scores: List[Int] = List.empty[Int], garbageCharCounter: Int = 0) {
     def collectScore(score: Int): Statistics = {
@@ -107,6 +95,7 @@ object Day09 {
   object Part2 {
     def solve(input: List[Char]): Int = {
       val finalState = StateMachine(input).run
+      assert(finalState.isInstanceOf[OutOfGroup], s"finalState.isInstanceOf[OutOfGroup] failed")
       finalState.stats.garbageCharCounter
     }
   }
--- src/test/scala/Day09Spec.scala
+++ src/test/scala/Day09Spec.scala
@@ -9,30 +9,7 @@ class Day09Spec extends FlatSpec with Matchers {
     Day09.input.take(10) should be("{{{{{},{<>".toList)
   }
 
-  behavior of "score()"
-  it should "return the right number of groups" in {
-    Day09.score("{}".toList)._1.size shouldBe 1
-    Day09.score("{{{}}}".toList)._1.size shouldBe 3
-    Day09.score("{{},{}}".toList)._1.size shouldBe 3
-    Day09.score("{{{},{},{{}}}}".toList)._1.size shouldBe 6
-    Day09.score("{<{},{},{{}}>}".toList)._1.size shouldBe 1
-    Day09.score("{<a>,<a>,<a>,<a>}".toList)._1.size shouldBe 1
-    Day09.score("{{<a>},{<a>},{<a>},{<a>}}".toList)._1.size shouldBe 5
-    Day09.score("{{<!>},{<!>},{<!>},{<a>}}".toList)._1.size shouldBe 2
-  }
-
   behavior of "solve() - Part1"
-  it should "solve the testcase(s)" in {
-    Day09.Part1.solve("{}".toList) shouldBe 1
-    Day09.Part1.solve("{{{}}}".toList) shouldBe 6
-    Day09.Part1.solve("{{},{}}".toList) shouldBe 5
-    Day09.Part1.solve("{{{},{},{{}}}}".toList) shouldBe 16
-    Day09.Part1.solve("{<a>,<a>,<a>,<a>}".toList) shouldBe 1
-    Day09.Part1.solve("{{<ab>},{<ab>},{<ab>},{<ab>}}".toList) shouldBe 9
-    Day09.Part1.solve("{{<!!>},{<!!>},{<!!>},{<!!>}}".toList) shouldBe 9
-    Day09.Part1.solve("{{<a!>},{<a!>},{<a!>},{<ab>}}".toList) shouldBe 3
-  }
-
   it should "be able to process garbage" in {
     Day09.Part1.solve("{<>}".toList) shouldBe 1
     Day09.Part1.solve("{<endanbca{dhbch}{{{{{!!!dndcn}}}}}}}}}<<<<<>}".toList) shouldBe 1
@@ -43,6 +20,27 @@ class Day09Spec extends FlatSpec with Matchers {
     Day09.Part1.solve("{<{o\"i!a,<{i<a>}".toList) shouldBe 1
   }
 
+  it should "solve the testcase(s)" in {
+    val testcases = List(
+      ("{}", 1, 1),
+      ("{{{}}}", 6, 3),
+      ("{{},{}}", 5, 3),
+      ("{{{},{},{{}}}}", 16, 6),
+      ("{<a>,<a>,<a>,<a>}", 1, 1),
+      ("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9, 5),
+      ("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9, 5),
+      ("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3, 2)
+    )
+
+    testcases.foreach { tc => {
+      val (stream, sum, size) = tc
+      val finalState = Day09.StateMachine(stream.toList).run
+      finalState shouldBe a [Day09.OutOfGroup]
+      finalState.stats.scores.sum shouldBe sum
+      finalState.stats.scores.size shouldBe size
+    }}
+  }
+
   it should "solve the puzzle" taggedAs (SolutionTest) in {
     Day09.Part1.solve(Day09.input) shouldBe 10800
   }
@@ -50,31 +48,5 @@ class Day09Spec extends FlatSpec with Matchers {
   behavior of "solve() - Part2"
   it should "solve the puzzle" taggedAs (SolutionTest) in {
     Day09.Part2.solve(Day09.input) shouldBe 4522
-  }
-
-  behavior of "run() - Part1"
-  it should "solve the testcase(s)" in {
-    val testcases = List(
-      ("{}", 1),
-      ("{{{}}}", 6),
-      ("{{},{}}", 5),
-      ("{{{},{},{{}}}}", 16),
-      ("{<a>,<a>,<a>,<a>}", 1),
-      ("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9),
-      ("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9),
-      ("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3)
-    )
-
-    testcases.foreach { tc => {
-      val (stream, expectedResult) = tc
-      val finalState = Day09.StateMachine(stream.toList).run
-      finalState.stats.scores.sum shouldBe expectedResult
-    }}
-  }
-
-  it should "solve the puzzle" in {
-    val finalState = Day09.StateMachine(Day09.input).run
-    finalState shouldBe a [Day09.OutOfGroup]
-    finalState.stats.scores.sum shouldBe 10800
   }
 }
