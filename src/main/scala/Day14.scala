b16f941d9da5989c49403017958055c4a7a39347
Roland Tritsch <roland@gonito.com>
2018-03-20 06:07:53 +0000

    Day14: Migrating to a grid of booleans (to make Part2 easier).

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -5,9 +5,10 @@ package aoc
   * Solution:
   *
   * General - Using the solution from Day10, we are calculating
-  * the 128x128 bit hash grid.
+  * the 128x128 bit hash grid. Note: Instead of a grid of chars
+  * [#.] I am using a grid of booleans.
   *
-  * Part1 - We are counting all the '1's in the hashes ('1' == '#').
+  * Part1 - We are counting all the trues in the hashes.
   *
   * Part2 -
   */
@@ -15,8 +16,9 @@ object Day14 {
 
   val input = Util.readInput("Day14input.txt").head
 
-  def buildGrid(input: String): List[String] = {
-    (0 to 127).map(row => Day10.dense2hex(Day10.dense(Day10.sparse(Day10.encode(s"${input}-${row}", Day10.suffix), Day10.seed, Day10.rounds).hash))).toList
+  def buildGrid(input: String): List[List[Boolean]] = {
+    val grid = (0 to 127).map(row => Day10.dense2hex(Day10.dense(Day10.sparse(Day10.encode(s"${input}-${row}", Day10.suffix), Day10.seed, Day10.rounds).hash))).toList
+    grid.map(hex2bin(_)).map(row => row.map(_ == '1').toList)
   }
 
   def hex2bin(hex: String): String = {
@@ -25,14 +27,14 @@ object Day14 {
 
   object Part1 {
     def solve(input: String): Int = {
-      val grid = buildGrid(input).map(hex2bin(_))
-      grid.foldLeft(0)((sum, hash) => sum + hash.count(_ == '1'))
+      val grid = buildGrid(input)
+      grid.foldLeft(0)((sum, hash) => sum + hash.count(identity))
     }
   }
 
   object Part2 {
     def solve(input: String): Int = {
-      val grid = buildGrid(input).map(hex2bin(_))
+      val grid = buildGrid(input)
 
       0
     }
