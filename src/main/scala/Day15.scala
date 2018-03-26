a40e73e9f4777717db7d66bd2aa02f10cdf88555
Roland Tritsch <roland@tritsch.org>
2018-03-26 14:10:39 +0100

    Day15: Refactoring. Part1/2. Based on iterator (not stream). Using recursion. Done.

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -16,49 +16,65 @@ object Day15 {
   // I just copied the values from it :).
 
   object Default {
-    val factorA = 16807
-    val factorB = 48271
-    val devider = Int.MaxValue // 2147483647
-    val depth = 40000000
-    val depth2 = 5000000
-
     val startA = 703
     val startB = 516
+
+    val factorA = 16807
+    val factorB = 48271
+
+    val devider = Int.MaxValue // 2147483647
+
+    val modolo = 1
+    val modoloA2 = 4
+    val modoloB2 = 8
+
+    def next(c: Long, f: Long, d: Long, m: Long): Long = {
+      val n = (c * f) % d
+      if(n % m == 0) n
+      else next(n, f, d, m)
+    }
+
+    val depth = 40000000
+    val depth2 = 5000000
   }
 
-  case class Generator(previous: Long, factor: Long, devider: Long = Default.devider) {
-    def numbers: Stream[Long] = next((previous * factor) % devider, factor, devider)
-    def next(p: Long, f: Long, d: Long): Stream[Long] = p #:: next((p*f) % d, f, d)
-  }
+  case class GeneratorConfig(start: Long, factor: Long, devider: Long, modolo: Long, next: (Long, Long, Long, Long) => Long)
 
-  case class BinaryPairGenerator(genA: Generator, genB: Generator) {
-    val numbers: Stream[(Long, Long)] = genA.numbers.zip(genB.numbers)
+  def generator(a: GeneratorConfig, b: GeneratorConfig): Iterator[(Long, Long)] = {
+    Iterator.iterate(a.start, b.start)(current => (a.next(current._1, a.factor, a.devider, a.modolo), b.next(current._2, b.factor, b.devider, b.modolo)))
   }
 
   def matching(pair: (Long, Long)): Boolean = {
-    val (thiz, thaz) = pair
-    val mask = 0xffff
-    (thiz & mask) == (thaz & mask)
+    (pair._1 & 0xffff) == (pair._2 & 0xffff)
   }
 
-  def countMatchingPair(binPairs: Stream[(Long, Long)], depth: Int): Int = {
-    binPairs.take(depth).count(matching(_))
+  def countMatchingPairs(gen: Iterator[(Long, Long)], depth: Int): Int = {
+    def go(g: Iterator[(Long, Long)], d: Int, c: Int): Int = {
+      if(d <= 0) c
+      else if(matching(g.next)) go(g, d - 1, c + 1)
+      else go(g, d - 1, c)
+    }
+
+    go(gen, depth, 0)
   }
 
   object Part1 {
     def solve: Int = {
-      val genA = Generator(Default.startA, Default.factorA)
-      val genB = Generator(Default.startB, Default.factorB)
+      val genA1 = GeneratorConfig(Default.startA, Default.factorA, Default.devider, Default.modolo, Default.next)
+      val genB1 = GeneratorConfig(Default.startB, Default.factorB, Default.devider, Default.modolo, Default.next)
+      val gen = generator(genA1, genB1)
 
-      val binPairGen = BinaryPairGenerator(genA, genB)
-
-      countMatchingPair(binPairGen.numbers, 4000000)
+      countMatchingPairs(gen, Default.depth)
     }
   }
 
   object Part2 {
     def solve: Int = {
-      0
+      val genA2 = GeneratorConfig(Default.startA, Default.factorA, Default.devider, Default.modoloA2, Default.next)
+      val genB2 = GeneratorConfig(Default.startB, Default.factorB, Default.devider, Default.modoloB2, Default.next)
+      val gen = generator(genA2, genB2)
+
+      countMatchingPairs(gen, Default.depth2)
     }
   }
 }
--- src/test/scala/Day15Spec.scala
+++ src/test/scala/Day15Spec.scala
@@ -1,46 +1,14 @@
 package aoc
 
-import aoc.Day15.Generator
 import org.scalatest.{FlatSpec, Matchers}
 
 class Day15Spec extends FlatSpec with Matchers {
 
-  val testGenA = List(
-    1092455,
-    1181022009,
-    245556042,
-    1744312007,
-    1352636452
-  )
-
-  val testGenB = List(
-    430625591,
-    1233683848,
-    1431495498,
-    137874439,
-    285222916
-  )
-
-  val testBinPairs = List(
-    (
-      "00000000000100001010101101100111",
-      "00011001101010101101001100110111"
-    ),(
-      "01000110011001001111011100111001",
-      "01001001100010001000010110001000"
-    ),(
-      "00001110101000101110001101001010",
-      "01010101010100101110001101001010"
-    ),(
-      "01100111111110000001011011000111",
-        "00001000001101111100110000000111"
-    ),(
-      "01010000100111111001100000100100",
-      "00010001000000000010100000000100"
-    )
-  )
+  val testAstart = 65
+  val testBstart = 8921
 
   val testDecPairs = List(
+    (testAstart, testBstart),
     (1092455,430625591),
     (1181022009,1233683848),
     (245556042,1431495498),
@@ -49,6 +17,7 @@ class Day15Spec extends FlatSpec with Matchers {
   )
 
   val test2DecPairs = List(
+    (testAstart, testBstart),
     (1352636452, 1233683848),
     (1992081072, 862516352),
     (530830436, 1159784568),
@@ -56,64 +25,37 @@ class Day15Spec extends FlatSpec with Matchers {
     (740335192, 412269392)
   )
 
-  val test2BinPairs = List(
-    (
-      "01010000100111111001100000100100",
-      "01001001100010001000010110001000"
-    ),(
-      "01110110101111001011111010110000",
-      "00110011011010001111010010000000"
-    ),(
-      "00011111101000111101010001100100",
-      "01000101001000001110100001111000"
-    ),(
-      "01110110000001001010100110110000",
-      "01100000010100110001010101001000"
-    ),(
-      "00101100001000001001111001011000",
-      "00011000100100101011101101010000"
-    )
-  )
-
-  val testAstart = 65
-  val testBstart = 8921
-
-  behavior of "a Generator()"
-  it should "produce a/the correct stream of numbers" in {
-    val genA = Day15.Generator(testAstart, Day15.Default.factorA)
-    genA.numbers.take(5).toList shouldBe testGenA
-
-    val genB = Day15.Generator(testBstart, Day15.Default.factorB)
-    genB.numbers.take(5).toList shouldBe testGenB
+  behavior of "generator()"
+  it should "produce the correct pairs of numbers for the testcase(s)" in {
+    val genA = Day15.GeneratorConfig(testAstart, Day15.Default.factorA, Day15.Default.devider, Day15.Default.modolo, Day15.Default.next)
+    val genB = Day15.GeneratorConfig(testBstart, Day15.Default.factorB, Day15.Default.devider, Day15.Default.modolo, Day15.Default.next)
+    Day15.generator(genA, genB).take(6).toList should be (testDecPairs)
   }
 
-  it should "produce the right pairs of binary numbers" taggedAs(BuildTest) in {
-    val genA = Day15.Generator(testAstart, Day15.Default.factorA)
-    val genB = Day15.Generator(testBstart, Day15.Default.factorB)
+  behavior of "countMatchingPairs()"
+  it should "return the correct result(s) for the testcase(s)" taggedAs (BuildTest) in {
+    val genA = Day15.GeneratorConfig(testAstart, Day15.Default.factorA, Day15.Default.devider, Day15.Default.modolo, Day15.Default.next)
+    val genB = Day15.GeneratorConfig(testBstart, Day15.Default.factorB, Day15.Default.devider, Day15.Default.modolo, Day15.Default.next)
+    val gen3 = Day15.generator(genA, genB)
+    Day15.countMatchingPairs(gen3, 3) shouldBe 0
 
-    val binPairGen = Day15.BinaryPairGenerator(genA, genB)
-    binPairGen.numbers.take(5).toList should be (testDecPairs)
-  }
+    val gen4 = Day15.generator(genA, genB)
+    Day15.countMatchingPairs(gen4, 4) shouldBe 1
 
-  behavior of "countMatchingPair()"
-  it should "return the correct result(s)" taggedAs(SlowTest) in {
-    val genA = Day15.Generator(testAstart, Day15.Default.factorA)
-    val genB = Day15.Generator(testBstart, Day15.Default.factorB)
+    val gen1000000 = Day15.generator(genA, genB)
+    Day15.countMatchingPairs(gen1000000, 1000000) shouldBe 12
 
-    val binPairGen = Day15.BinaryPairGenerator(genA, genB)
-
-    Day15.countMatchingPair(binPairGen.numbers, 2) shouldBe 0
-    Day15.countMatchingPair(binPairGen.numbers, 3) shouldBe 1
-    Day15.countMatchingPair(binPairGen.numbers, 4000000) shouldBe 588
+    val genDefaultDepth = Day15.generator(genA, genB)
+    Day15.countMatchingPairs(genDefaultDepth, Day15.Default.depth) shouldBe 588
   }
 
   behavior of "solve() - Part1"
-  ignore should "solve the puzzle" taggedAs(SolutionTest) in {
+  it should "solve the puzzle" taggedAs(SolutionTest) in {
     Day15.Part1.solve shouldBe 594
   }
 
   behavior of "solve() - Part2"
-  ignore should "solve the puzzle" taggedAs(SolutionTest) in {
+  it should "solve the puzzle" taggedAs(SolutionTest) in {
     Day15.Part2.solve shouldBe 328
   }
 }
