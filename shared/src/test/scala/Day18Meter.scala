a3aa7cfdce01ecf4f8b2414542adf940da560a49
Roland Tritsch <roland@tritsch.org>
2018-04-16 07:44:44 +0100

    Day18: Refactoring. Adding Meter test.

---
+++ shared/src/test/scala/Day18Meter.scala
@@ -0,0 +1,21 @@
+package aoc
+
+import org.scalameter.api._
+import org.scalameter.picklers.Implicits._
+
+object Day18Meter extends Bench.OfflineReport {
+
+  val nGen = for {
+    n <- Gen.range("n")(1, 5, 1)
+  } yield {
+    n
+  }
+
+  performance of "Part1" in {
+    measure method "solve" in {
+      using(nGen) in {
+        _ => Day18.Part1.solve(Day18.input)
+      }
+    }
+  }
+}
