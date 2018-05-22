23cff9de1726736b13c1c2e4587e377d4567258a
Roland Tritsch <roland@tritsch.org>
2018-05-22 20:37:35 +0100

    Day22: Refactoring. Adding documentation.

--- shared/src/main/scala/Day22.scala
+++ shared/src/main/scala/Day22.scala
@@ -4,11 +4,13 @@ package aoc
   *
   * Solution:
   *
-  * General - ???
+  * General - Basicly I am implementing two grids (with a static/
+  * pre-allocated size (not good; needs to be fixed)) and walk
+  * these grids follwoing the rules in the problem statement.
   *
-  * Part1 - ???
+  * Part1 - Walk the [[SimpleGrid]] and count the infections.
   *
-  * Part2 - ???
+  * Part2 - Walk the [[AdvancedGrid]] and count the infections.
   *
   */
 
