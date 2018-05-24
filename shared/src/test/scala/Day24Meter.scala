36f0c3d8fc6769e4ff8022d9849a651bf69fad81
Roland Tritsch <rtritsch@tenable.com>
2018-05-24 08:47:29 +0100

    Day24: Refactoring. Adding Check and Meter (kind of).

---
+++ shared/src/test/scala/Day24Meter.scala
@@ -0,0 +1,29 @@
+package aoc
+
+import org.scalameter.api._
+import org.scalameter.picklers.Implicits._
+
+object Day24Meter extends Bench.OfflineReport {
+
+  val nGen = for {
+    n <- Gen.range("n")(1, 1, 1)
+  } yield {
+    n
+  }
+
+  performance of "Part1" in {
+    measure method "solve" in {
+      using(nGen) in {
+        _ => Day24.Part1.solve(Day24.input)
+      }
+    }
+  }
+
+  performance of "Part2" in {
+    measure method "solve" in {
+      using(nGen) in {
+        _ => Day24.Part2.solve(Day24.input)
+      }
+    }
+  }
+}
