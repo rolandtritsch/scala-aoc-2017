0a157434379811f799f1e546d07a6ae5862d3317
Roland Tritsch <rtritsch@tenable.com>
2018-05-26 07:46:54 +0100

    Day25: Does not compile for native.

---
+++ native/src/main/scala/Day25.scala
@@ -0,0 +1,26 @@
+package aoc
+
+/** Dummy implementation to make Main compile for native.
+  *
+  * Day25 does not compile with/for native. The error message is ...
+  * {{{
+  * 'init_scala.collection.mutable.ArrayBuffer' defined with type 'void (i8*, i8*)*'
+  * }}}
+  * ... and I have not figured out (yet) what is wrong.
+  */
+object Day25 {
+
+  val input = List.empty[String]
+
+  object Part1 {
+    def solve(input: List[String]): Int = {
+      0
+    }
+  }
+
+  object Part2 {
+    def solve(input: List[String]): Int = {
+      0
+    }
+  }
+}
